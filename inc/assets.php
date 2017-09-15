d2bc2a186a72290f29e14d2c6b2d88cf7e8fd0c6
Felix Arntz <felix-arntz@leaves-and-love.net>
2017-09-15 22:58:24 +0200

    Compile JS and enqueue concatenated version.

--- inc/assets.php
+++ inc/assets.php
@@ -13,15 +13,14 @@
  * @since 1.0.0
  */
 function super_awesome_theme_enqueue_assets() {
+	$min = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '': '.min';
+
 	wp_enqueue_style( 'super-awesome-theme-style', get_stylesheet_uri() );
 
-	// Load the html5 shiv.
+	wp_enqueue_script( 'super-awesome-theme-script', get_theme_file_uri( '/assets/js/theme' . $min . '.js' ), array(), SUPER_AWESOME_THEME_VERSION, true );
+
 	wp_enqueue_script( 'html5', get_theme_file_uri( '/assets/js/html5.js' ), array(), '3.7.3' );
 	wp_script_add_data( 'html5', 'conditional', 'lt IE 9' );
-
-	wp_enqueue_script( 'super-awesome-theme-navigation', get_theme_file_uri( '/assets/js/navigation.js' ), array(), SUPER_AWESOME_THEME_VERSION, true );
-
-	wp_enqueue_script( 'super-awesome-theme-skip-link-focus-fix', get_theme_file_uri( '/assets/js/skip-link-focus-fix.js' ), array(), SUPER_AWESOME_THEME_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
