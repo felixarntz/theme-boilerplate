6f0a5b2e4496c1b32336fb7b1bc062a531781680
Felix Arntz <felix-arntz@leaves-and-love.net>
2017-09-15 21:36:31 +0200

    Include package.json and gulpfile.js.

---
+++ gulpfile.js
@@ -0,0 +1,209 @@
+/* ---- THE FOLLOWING CONFIG SHOULD BE EDITED ---- */
+
+var pkg = require( './package.json' );
+
+var config = {
+	textDomain: 'super-awesome-theme',
+	domainPath: '/languages/',
+	themeName: 'Super Awesome Theme',
+	themeURI: pkg.homepage,
+	author: pkg.author.name,
+	authorURI: pkg.author.url,
+	description: pkg.description,
+	version: pkg.version,
+	license: 'GNU General Public License v3 or later',
+	licenseURI: 'https://www.gnu.org/licenses/gpl-3.0.html',
+	tags: pkg.keywords.join( ', ' ),
+	contributors: [ 'flixos90', 'philliproth' ].join( ', ' ),
+	minRequired: '4.7',
+	testedUpTo: '4.8',
+	translateURI: 'https://translate.wordpress.org/projects/wp-themes/super-awesome-theme'
+};
+
+/* ---- DO NOT EDIT BELOW THIS LINE ---- */
+
+// WP theme header for style.css
+var themeheader =	'/*!\n' +
+					'Theme Name: ' + config.themeName + '\n' +
+					'Theme URI: ' + config.themeURI + '\n' +
+					'Author: ' + config.author + '\n' +
+					'Author URI: ' + config.authorURI + '\n' +
+					'Description: ' + config.description + '\n' +
+					'Version: ' + config.version + '\n' +
+					'License: ' + config.license + '\n' +
+					'License URI: ' + config.licenseURI + '\n' +
+					'Text Domain: ' + config.textDomain + '\n' +
+					( config.domainPath ? 'Domain Path: ' + config.domainPath + '\n' : '' ) +
+					'Tags: ' + config.tags;
+
+// WP theme header for readme.txt
+var readmeheader =	'/*\n' +
+					'Contributors: ' + config.contributors + '\n' +
+					'Version: ' + config.version + '\n' +
+					'Requires at least: ' + config.minRequired + '\n' +
+					'Tested up to: ' + config.testedUpTo + '\n' +
+					'Stable tag: ' + config.version + '\n' +
+					'Version: ' + config.version + '\n' +
+					'License: ' + config.license + '\n' +
+					'License URI: ' + config.licenseURI + '\n' +
+					'Tags: ' + config.tags;
+
+
+/* ---- REQUIRED DEPENDENCIES ---- */
+
+var gulp = require( 'gulp' );
+
+var cleanCss = require( 'gulp-clean-css' );
+var concat   = require( 'gulp-concat' );
+var csscomb  = require( 'gulp-csscomb' );
+var jshint   = require( 'gulp-jshint' );
+var rename   = require( 'gulp-rename' );
+var replace  = require( 'gulp-replace' );
+var sass     = require( 'gulp-sass' );
+var sort     = require( 'gulp-sort' );
+var uglify   = require( 'gulp-uglify' );
+var wpPot    = require( 'gulp-wp-pot' );
+
+/* ---- MAIN TASKS ---- */
+
+// default task (compile Sass and JavaScript and refresh POT file)
+gulp.task( 'default', [ 'sass', 'js', 'pot' ]);
+
+// build the theme
+gulp.task( 'build', [ 'header-replace', 'readme-replace' ], function() {
+	gulp.start( 'default' );
+});
+
+/* ---- SUB TASKS ---- */
+
+// compile Sass
+gulp.task( 'sass', function( done ) {
+	gulp.src( './assets/sass/style.scss' )
+		.pipe( sass({
+			errLogToConsole: true,
+			outputStyle: 'expanded'
+		}) )
+		.pipe( csscomb() )
+		.pipe( gulp.dest( './' ) )
+		.on( 'end', done );
+});
+
+// compile JavaScript
+gulp.task( 'js', function( done ) {
+	var builderFiles = [
+		'./assets/js/**/*.js',
+		'!./assets/js/customize-preview.js',
+		'!./assets/js/customize-preview.min.js',
+		'!./assets/js/html5.js',
+		'!./assets/js/theme.js',
+		'!./assets/js/theme.min.js'
+	];
+
+	gulp.src( builderFiles )
+		.pipe( jshint() )
+		.pipe( jshint.reporter( 'default' ) )
+		.pipe( jscs() )
+		.pipe( jscs.reporter() )
+		.pipe( concat( 'theme.js' ) )
+		.pipe( banner( assetheader ) )
+		.pipe( gulp.dest( './assets/js/' ) )
+		.pipe( uglify() )
+		.pipe( rename({
+			extname: '.min.js'
+		}) )
+		.pipe( gulp.dest( './assets/js/' ) )
+		.on( 'end', function() {
+			gulp.src( './assets/js/customize-preview.js' )
+				.pipe( jshint() )
+				.pipe( jshint.reporter( 'default' ) )
+				.pipe( jscs() )
+				.pipe( jscs.reporter() )
+				.pipe( uglify() )
+				.pipe( rename({
+					extname: '.min.js'
+				}) )
+				.pipe( gulp.dest( './assets/js/' ) )
+				.on( 'end', done );
+		});
+});
+
+// generate POT file
+gulp.task( 'pot', function( done ) {
+	var phpFiles = [
+		'./*.php',
+		'./inc/**/*.php',
+		'./template-parts/**/*.php'
+	];
+
+	gulp.src( phpFiles, { base: './' })
+		.pipe( sort() )
+		.pipe( wpPot({
+			domain: config.textDomain,
+			destFile: './languages/' + config.textDomain + '.pot',
+			headers: {
+				'report-msgid-bugs-to': config.translateURI,
+				'x-generator': 'gulp-wp-pot',
+				'x-poedit-basepath': '.',
+				'x-poedit-language': 'English',
+				'x-poedit-country': 'UNITED STATES',
+				'x-poedit-sourcecharset': 'uft-8',
+				'x-poedit-keywordslist': '__;_e;_x:1,2c;_ex:1,2c;_n:1,2; _nx:1,2,4c;_n_noop:1,2;_nx_noop:1,2,3c;esc_attr__; esc_html__;esc_attr_e; esc_html_e;esc_attr_x:1,2c; esc_html_x:1,2c;',
+				'x-poedit-bookmars': '',
+				'x-poedit-searchpath-0': '.',
+				'x-textdomain-support': 'yes'
+			}
+		}) )
+		.pipe( gulp.dest( './' ) )
+		.on( 'end', done );
+});
+
+// replace the theme header in assets/sass/style.scss
+gulp.task( 'header-replace', function( done ) {
+	gulp.src( './assets/sass/style.scss' )
+		.pipe( replace( /^\/\*!\s(.+)WordPress Theme, Copyright \(C\)/, '/*!\n' + themeheader + '\n\n' + config.themeName + 'WordPress Theme, Copyright (C)' ) )
+		.pipe( gulp.dest( './' ) )
+		.on( 'end', done );
+});
+
+// replace the theme header in readme.txt
+gulp.task( 'readme-replace', function( done ) {
+	gulp.src( './readme.txt' )
+		.pipe( replace( /\=\=\= (.+) \=\=\=([\s\S]+)\=\= Description \=\=/m, '=== ' + config.themeName + ' ===\n\n' + readmeheader + '\n\n== Description ==' ) )
+		.pipe( gulp.dest( './' ) )
+		.on( 'end', done );
+});
+
+/* ---- INITIAL SETUP TASK: Change the replacements, run the command once and then delete it from here ---- */
+
+gulp.task( 'init-replace', function( done ) {
+	var replacements = {
+		'super-awesome-theme': 'my-new-theme-name',
+		'SUPER_AWESOME_THEME': 'MY_NEW_THEME_NAME',
+		'super_awesome_theme': 'my_new_theme_name',
+		'Super_Awesome_Theme': 'My_New_Theme_Name',
+		'Super Awesome Theme': 'My New Theme Name',
+		'SuperAwesomeTheme'  : 'MyNewThemeName',
+		'superAwesomeTheme'  : 'myNewThemeName'
+	};
+
+	var files = [
+		'./*.php',
+		'./inc/**/*.php',
+		'./template-parts/**/*.php',
+		'./assets/js/**/*.js',
+		'./assets/sass/style.scss',
+		'./style.css',
+		'./gulpfile.js'
+	];
+
+	gulp.src( files, { base: './' })
+		.pipe( replace( 'super-awesome-theme', replacements['super-awesome-theme'] ) )
+		.pipe( replace( 'SUPER_AWESOME_THEME', replacements['SUPER_AWESOME_THEME'] ) )
+		.pipe( replace( 'super_awesome_theme', replacements['super_awesome_theme'] ) )
+		.pipe( replace( 'Super_Awesome_Theme', replacements['Super_Awesome_Theme'] ) )
+		.pipe( replace( 'Super Awesome Theme', replacements['Super Awesome Theme'] ) )
+		.pipe( replace( 'SuperAwesomeTheme',   replacements['SuperAwesomeTheme'] ) )
+		.pipe( replace( 'superAwesomeTheme',   replacements['superAwesomeTheme'] ) )
+		.pipe( gulp.dest( './' ) )
+		.on( 'end', done );
+});
--- readme.txt
+++ readme.txt
@@ -1,38 +1,63 @@
 === Super Awesome Theme ===
 
-Contributors: automattic
-Tags: custom-background, custom-logo, custom-menu, featured-images, threaded-comments, translation-ready
-
-Requires at least: 4.0
-Tested up to: 4.8
+Contributors: flixos90, philliproth
 Stable tag: 1.0.0
-License: GNU General Public License v2 or later
-License URI: LICENSE
-
-A starter theme called Super Awesome Theme.
+Version: 1.0.0
+Requires at least: 4.7
+Tested up to: 4.8
+License: GNU General Public License v3 or later
+License URI: https://www.gnu.org/licenses/gpl-3.0.html
+Tags: one-column, two-columns, left-sidebar, right-sidebar, flexible-header, accessibility-ready, custom-background, custom-colors, custom-header, custom-menu, custom-logo, editor-style, featured-image-header, featured-images, footer-widgets, full-width-template, post-formats, rtl-language-support, theme-options, threaded-comments, translation-ready
 
 == Description ==
 
-This is my super awesome theme.
+Super Awesome Theme theme for WordPress.
 
 == Installation ==
 
-1. In your admin panel, go to Appearance > Themes and click the Add New button.
-2. Click Upload and Choose File, then select the theme's .zip file. Click Install Now.
-3. Click Activate to use your new theme right away.
+1. In your admin panel, go to Appearance -> Themes and click the 'Add New' button.
+2. Type in 'Super Awesome Theme' in the search form and press the 'Enter' key on your keyboard.
+3. Click on the 'Activate' button to use your new theme right away.
+4. Navigate to Appearance > Customize in your admin panel and customize to taste.
 
 == Frequently Asked Questions ==
 
 = Does this theme support any plugins? =
 
-Super Awesome Theme includes support for Infinite Scroll in Jetpack.
+Super Awesome Theme supports the following plugins:
+
+* [Easy Digital Downloads](https://wordpress.org/plugins/easy-digital-downloads/)
+* [Give](https://wordpress.org/plugins/give/)
+* [Jetpack](https://wordpress.org/plugins/jetpack/)
+* [Torro Forms](https://wordpress.org/plugins/torro-forms/)
+* [WP Event Calendar](https://wordpress.org/plugins/wp-event-calendar/)
+* [WP Offload S3](https://wordpress.org/plugins/amazon-s3-and-cloudfront/)
+
+== Copyright ==
+
+Super Awesome Theme WordPress Theme, Copyright (C) 2017 Taco Themes
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+Super Awesome Theme bundles the following third-party resources:
+
+HTML5 Shiv, Copyright 2014 Alexander Farkas
+Licenses: MIT/GPL2
+Source: https://github.com/aFarkas/html5shiv
+
+normalize.css, Copyright 2012-2016 Nicolas Gallagher and Jonathan Neal
+License: MIT
+Source: https://necolas.github.io/normalize.css/
 
 == Changelog ==
 
-= 1.0 - May 12 2015 =
+= 1.0.0 =
 * Initial release
-
-== Credits ==
-
-* Based on Underscores http://underscores.me/, (C) 2012-2017 Automattic, Inc., [GPLv2 or later](https://www.gnu.org/licenses/gpl-2.0.html)
-* normalize.css http://necolas.github.io/normalize.css/, (C) 2012-2016 Nicolas Gallagher and Jonathan Neal, [MIT](http://opensource.org/licenses/MIT)
