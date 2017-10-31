f4dfb5127156065e4d572ae111be823ca9c52e5b
Felix Arntz <felix-arntz@leaves-and-love.net>
2017-10-31 16:02:09 -0500

    Introduce distraction free mode and template, plus a full width template.

---
+++ templates/distraction-free.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Template Name: Distraction Free Page
+ *
+ * Distraction-free template that uses the distraction-free primary navigation
+ * and displays neither a sidebar nor footer widgets.
+ *
+ * @package Super_Awesome_Theme
+ * @license GPL-3.0
+ * @link    https://www.taco-themes.com/themes/super-awesome-theme/
+ */
+
+// This file should match the `singular.php` template exactly.
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+		<?php
+		while ( have_posts() ) : the_post();
+
+			if ( super_awesome_theme_use_post_format_templates() ) :
+				get_template_part( 'template-parts/content/content-' . get_post_type(), get_post_format() );
+			else :
+				get_template_part( 'template-parts/content/content', get_post_type() );
+			endif;
+
+			if ( super_awesome_theme_display_post_navigation() ) :
+				the_post_navigation();
+			endif;
+
+			if ( super_awesome_theme_display_post_comments() ) :
+				comments_template();
+			endif;
+
+		endwhile; // End of the loop.
+		?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+if ( super_awesome_theme_display_sidebar() ) {
+	get_sidebar( super_awesome_theme_get_sidebar_name() );
+}
+
+get_footer();
