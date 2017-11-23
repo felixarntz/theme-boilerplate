c25ba73e14635299f3251a42667977886938a2da
Felix Arntz <felix-arntz@leaves-and-love.net>
2017-11-22 22:28:59 -0600

    Add template for page that requires login.

---
+++ templates/account.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Template Name: Account Page
+ *
+ * The account page template requires the user to be logged-in.
+ *
+ * @package Super_Awesome_Theme
+ * @license GPL-3.0
+ * @link    https://www.taco-themes.com/themes/super-awesome-theme/
+ */
+
+get_header();
+
+?>
+
+	<main id="main" class="site-main">
+
+		<?php
+		while ( have_posts() ) : the_post();
+
+			if ( ! is_user_logged_in() ) :
+				?>
+				<header class="page-header">
+					<?php the_title( '<h1>', '</h1>' ); ?>
+				</header><!-- .page-header -->
+
+				<div class="page-login-form">
+					<p><?php esc_html_e( 'You must be logged in to view the content of this page.', 'super-awesome-theme' ); ?></p>
+
+					<?php wp_login_form(); ?>
+
+					<p class="login-lost-password">
+						<a href="<?php echo esc_url( wp_lostpassword_url( get_permalink() ) ); ?>"><?php esc_html_e( 'Lost your password?', 'super-awesome-theme' ); ?></a>
+					</p>
+				</div>
+				<?php
+			else :
+				if ( super_awesome_theme_use_post_format_templates() ) :
+					get_template_part( 'template-parts/content/content-' . get_post_type(), get_post_format() );
+				else :
+					get_template_part( 'template-parts/content/content', get_post_type() );
+				endif;
+
+				if ( super_awesome_theme_display_post_navigation() ) :
+					the_post_navigation();
+				endif;
+
+				if ( super_awesome_theme_display_post_comments() ) :
+					comments_template();
+				endif;
+			endif;
+
+		endwhile; // End of the loop.
+		?>
+
+	</main><!-- #main -->
+
+<?php
+
+get_sidebar();
+get_footer();
