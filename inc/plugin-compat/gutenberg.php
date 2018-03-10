5426e09601de6aa2d70ff48982c34816b4195bd5
Felix Arntz <felix-arntz@leaves-and-love.net>
2018-03-10 11:55:24 -0500

    Improve Gutenberg support and fix some broken styles.

--- inc/plugin-compat/gutenberg.php
+++ inc/plugin-compat/gutenberg.php
@@ -13,15 +13,18 @@
  * @since 1.0.0
  */
 function super_awesome_theme_gutenberg_setup() {
+	$text_color       = get_theme_mod( 'text_color', '#404040' );
+	$link_color       = get_theme_mod( 'link_color', '#21759b' );
+	$link_hover_color = super_awesome_theme_darken_color( $link_color, 8 );
+
 	add_theme_support( 'align-wide' );
-	add_theme_support( 'editor-color-palette', array(
+	add_theme_support( 'disable-custom-colors' );
+	add_theme_support( 'editor-color-palette',
 		'#ffffff',
 		'#f1f1f1',
-		'#222222',
-		'#404040',
-		'#8f98a1',
-		'#e6e6e6',
-		'#21759b',
-	) );
+		$text_color,
+		$link_color,
+		$link_hover_color
+	);
 }
 add_action( 'after_setup_theme', 'super_awesome_theme_gutenberg_setup' );
