6d967a446c209e16fb41c8465d8c4e5ae62b23dc
Felix Arntz <felix-arntz@leaves-and-love.net>
2018-04-05 13:39:02 +0200

    Outsource site logo and title in its separate template to make it more reusable.

---
+++ template-parts/header/logo-and-title.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * The template for displaying the site logo and title in the header
+ *
+ * @package Super_Awesome_Theme
+ * @license GPL-2.0-or-later
+ * @link    https://super-awesome-author.org/themes/super-awesome-theme/
+ */
+
+$title       = get_bloginfo( 'name', 'display' );
+$description = get_bloginfo( 'description', 'display' );
+
+// The $data variable contains arbitrary data passed to the template.
+$skip_h1 = isset( $data ) && ! empty( $data['skip_h1'] );
+
+?>
+
+<?php the_custom_logo(); ?>
+
+<div class="site-branding-text">
+	<?php if ( ! empty( $title ) || is_customize_preview() ) : ?>
+		<?php if ( is_front_page() && is_home() && ! $skip_h1 ) : ?>
+			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php echo $title; /* WPCS: XSS OK. */ ?></a></h1>
+		<?php else : ?>
+			<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php echo $title; /* WPCS: XSS OK. */ ?></a></p>
+		<?php endif; ?>
+	<?php endif; ?>
+
+	<?php if ( ! empty( $description ) || is_customize_preview() ) : ?>
+		<p class="site-description"><?php echo $description; /* WPCS: XSS OK. */ ?></p>
+	<?php endif; ?>
+</div><!-- .site-branding-text -->
--- template-parts/header/site-branding.php
+++ template-parts/header/site-branding.php
@@ -10,19 +10,6 @@
 ?>
 <div class="site-branding site-component">
 	<div class="site-component-inner">
-		<?php
-		the_custom_logo();
-		if ( is_front_page() && is_home() ) : ?>
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-		<?php else : ?>
-			<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-		<?php
-		endif;
-
-		$description = get_bloginfo( 'description', 'display' );
-		if ( $description || is_customize_preview() ) : ?>
-			<p class="site-description"><?php echo $description; /* WPCS: XSS OK. */ ?></p>
-		<?php
-		endif; ?>
+		<?php get_template_part( 'template-parts/header/logo-and-title' ); ?>
 	</div>
 </div><!-- .site-branding -->
