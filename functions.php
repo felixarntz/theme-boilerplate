60ce7c7bdf85953f2560c7aa1421e5bd807f1b04
Felix Arntz <felix-arntz@leaves-and-love.net>
2018-05-02 10:55:23 +0200

    Initialize theme after all files have been required.

--- functions.php
+++ functions.php
@@ -32,13 +32,6 @@ if ( version_compare( $GLOBALS['wp_version'], '4.7', '<' ) ) {
  */
 require get_template_directory() . '/inc/library.php';
 
-super_awesome_theme()
-	->register_component( new Super_Awesome_Theme_Settings() )
-	->register_component( new Super_Awesome_Theme_Assets() )
-	->register_component( new Super_Awesome_Theme_Customizer() )
-	->resolve_dependencies()
-	->initialize();
-
 /**
  * Theme setup.
  */
@@ -78,3 +71,10 @@ require get_template_directory() . '/inc/customizer.php';
  * Plugin compatibility.
  */
 require get_template_directory() . '/inc/plugin-compat.php';
+
+super_awesome_theme()
+	->register_component( new Super_Awesome_Theme_Settings() )
+	->register_component( new Super_Awesome_Theme_Assets() )
+	->register_component( new Super_Awesome_Theme_Customizer() )
+	->resolve_dependencies()
+	->initialize();
