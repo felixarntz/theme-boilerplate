f5ee8a7723dba7344e77d78c1a1066844605bca9
Felix Arntz <felix-arntz@leaves-and-love.net>
2018-05-19 13:14:46 +0200

    Fix tests bootstrap script.

--- tests/phpunit/includes/bootstrap.php
+++ tests/phpunit/includes/bootstrap.php
@@ -12,33 +12,41 @@ if ( function_exists( 'xdebug_disable' ) ) {
 	xdebug_disable();
 }
 
+$needs_custom_theme_dir = false;
+
 if ( false !== getenv( 'WP_DEVELOP_DIR' ) ) {
 	$test_root = getenv( 'WP_DEVELOP_DIR' ) . '/tests/phpunit';
+
+	$needs_custom_theme_dir = true;
 } elseif ( file_exists( '/tmp/wordpress-tests-lib/includes/bootstrap.php' ) ) {
 	$test_root = '/tmp/wordpress-tests-lib';
+
+	$needs_custom_theme_dir = true;
 } else {
 	$test_root = '../../../../../../tests/phpunit';
 }
 
-define( 'WP_THEME_DIR', dirname( dirname( dirname( dirname( dirname( __FILE__ ) ) ) ) ) );
 define( 'WP_DEFAULT_THEME', 'super-awesome-theme' );
 
 $GLOBALS['wp_tests_options'] = array(
-	'template'        => 'super-awesome-theme',
-	'stylesheet'      => 'super-awesome-theme',
-	'template_root'   => WP_THEME_DIR,
-	'stylesheet_root' => WP_THEME_DIR,
+	'template'   => 'super-awesome-theme',
+	'stylesheet' => 'super-awesome-theme',
 );
 
-require_once $test_root . '/includes/functions.php';
+if ( $needs_custom_theme_dir ) {
+	$GLOBALS['wp_tests_options']['template_root']   = dirname( dirname( dirname( dirname( dirname( __FILE__ ) ) ) ) );
+	$GLOBALS['wp_tests_options']['stylesheet_root'] = $GLOBALS['wp_tests_options']['template_root'];
 
-/**
- * Registers the custom theme directory.
- */
-function test_register_theme_directory() {
-	register_theme_directory( WP_THEME_DIR );
+	require_once $test_root . '/includes/functions.php';
+
+	/**
+	 * Registers the custom theme directory.
+	 */
+	function test_register_theme_directory() {
+		register_theme_directory( $GLOBALS['wp_tests_options']['stylesheet_root'] );
+	}
+	tests_add_filter( 'setup_theme', 'test_register_theme_directory' );
 }
-tests_add_filter( 'setup_theme', 'test_register_theme_directory' );
 
 require_once $test_root . '/includes/bootstrap.php';
 
