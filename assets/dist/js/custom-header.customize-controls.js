74fbf559bbb1861e739f8e015bb9a7afe410ac81
Felix Arntz <felix-arntz@leaves-and-love.net>
2018-06-19 22:38:21 +0200

    Fix bug where Customizer controls would not be associated with their settings.

--- assets/dist/js/custom-header.customize-controls.js
+++ assets/dist/js/custom-header.customize-controls.js
@@ -102,6 +102,7 @@
 				});
 
 				api.control.add(new api.Control('branding_location', {
+					setting: 'branding_location',
 					section: 'title_tagline',
 					label: __('Display Location', 'super-awesome-theme'),
 					description: __('Specify where to display the site logo, title and tagline.', 'super-awesome-theme'),
@@ -110,6 +111,7 @@
 				}));
 
 				api.control.add(new api.Control('header_textalign', {
+					setting: 'header_textalign',
 					section: 'header_image',
 					label: _x('Text Alignment', 'custom header', 'super-awesome-theme'),
 					type: 'radio',
--- assets/src/js/custom-header.customize-controls.js
+++ assets/src/js/custom-header.customize-controls.js
@@ -14,6 +14,7 @@
 		});
 
 		api.control.add( new api.Control( 'branding_location', {
+			setting:     'branding_location',
 			section:     'title_tagline',
 			label:       __( 'Display Location', 'super-awesome-theme' ),
 			description: __( 'Specify where to display the site logo, title and tagline.', 'super-awesome-theme' ),
@@ -22,6 +23,7 @@
 		}) );
 
 		api.control.add( new api.Control( 'header_textalign', {
+			setting: 'header_textalign',
 			section: 'header_image',
 			label:   _x( 'Text Alignment', 'custom header', 'super-awesome-theme' ),
 			type:    'radio',
